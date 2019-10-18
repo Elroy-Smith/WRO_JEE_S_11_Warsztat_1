a04ab0920d307fb8af71055cf88fa4a7ceaf27e2
Honest IT Consulting <michal.kupisinski@gmail.com>
2019-10-18 15:26:48 +0200

    Zadanie1 rozwiazane

--- src/pl/coderslab/warsztat1/Main1.java
+++ src/pl/coderslab/warsztat1/Main1.java
@@ -1,8 +1,54 @@
 package pl.coderslab.warsztat1;
 
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main1 {
+    static int randomNumber() {
+        Random random = new Random();
+        int number = random.nextInt(10) + 1;
+        System.out.println(number);
+        return number;
+    }
+
+    static int getNumber(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Zgadnij liczbę:");
+        int number;
+        while (!scanner.hasNextInt()){
+            scanner.next();
+            System.out.println("To nie jest liczba");
+        }
+        number = scanner.nextInt();
+        return number;
+    }
+
+        static boolean check(int randomNumber) {
+
+        int getNumber = getNumber();
+
+
+        if ( getNumber< randomNumber){
+            System.out.println("Za mało");
+            return false;
+        } else if (getNumber>randomNumber) {
+            System.out.println("Za dużo");
+
+            return false;
+        }else {
+            System.out.println("Zgadłeś");
+            return true;
+        }
+
+        }
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+       int randomNumber=randomNumber();
+       while (!check(randomNumber));
+
     }
+
+
+
 }
